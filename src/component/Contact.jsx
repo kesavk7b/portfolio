624904cb57eb5415980816ccda7a553d2ec18fe4
kesavk7b <kesavabharathi2001@gmail.com>
2024-02-18 22:31:51 +0530

    update contact alignment

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRobot } from '@fortawesome/free-solid-svg-icons';
 export default function Contact({icon=faRobot,children,title=""}){
     return(
-        <div title={title} className="p-2 col-1 col-sm-12 col-lg-3">
+        <div title={title} className="p-2 col-12 col-sm-12 col-md-3 col-lg-3">
             <div className='flex'>
                 <FontAwesomeIcon size='2x' icon={icon} />
             </div>
